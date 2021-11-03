1f90dbc552d3847be6625057040b20fe59cb916a
Ahmad770-ux <78217646+Ahmad770-ux@users.noreply.github.com>
2021-11-03 13:11:59 -0700

    Add files via upload

---
+++ eurika/client/demo/src/main/java/com/example/demo/ClientController.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController {
+@Value("${spring.profiles}")
+private String zone;
+@GetMapping("/ping")
+public String ping() {
+return "I'm in zone " + zone;
+}
+}
